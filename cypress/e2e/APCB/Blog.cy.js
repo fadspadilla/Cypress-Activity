92f297c009070422b61cfedd668cf72cc973c4ef
fadspaadilla <fadspadilla@gmail.com>
2024-09-25 14:24:30 +0800

    added new test cases but still in IP

--- cypress/e2e/APCB/Blog.cy.js
+++ cypress/e2e/APCB/Blog.cy.js
@@ -1,10 +1,28 @@
 describe("APCB Blog Tests", () => {
-    before(() => {
+    let blogFixture;
+    beforeEach(() => {
         cy.visit('/')        
         cy.get('nav > ul > li:nth-child(5)').click()
         cy.get('h1').should('contain', 'Blog')
+        cy.fixture("APCB/blogs.json").then((data) => {
+            blogFixture = data;
+        })
     })
-    it("Editor's Pick", () => {
+    it.only("Verify Blog Card on Featured Article is working", () => {
+        cy.get(".article-featured > div > a.card-hit-zone > span").then(($text) => {
+            cy.wrap($text).invoke('text').then(($cardTitle) => {
+                const cardTitle = $cardTitle.trim();
+                cy.get(".article-featured > div > a.card-button").click().then(($link) => {
+                    const href = $link.attr('href')
+                    cy.url().should('contain', href) //assert link
+                    cy.get('h1').should('contain', cardTitle) // assert title
+                    cy.get('.raw.mb-8 > p:nth-child(1)').should('not.be.empty') //assert not empty           
+                })
+            })
+
+        })  
+    })
+    it.skip("Verify Blog Cards on Editor's Picks section are working", () => {
         cy.get('.my-12  div.article-card-group > div').each(($el, $index) => {
             cy.get(`.my-12  div.article-card-group > div:nth-child(${$index + 1}) > h4`).then(($text) => {
                 cy.wrap($text).invoke('text').then(($cardTitle) => {
@@ -13,6 +31,7 @@ describe("APCB Blog Tests", () => {
                         cy.get('h1').invoke('text').then(($text) => {
                             let h1 = $text.trim();
                             expect(h1).to.eq(cardTitle);
+                            cy.get('.raw.mb-8 > p:nth-child(1)').should('not.be.empty')
                             cy.go('back')
                         })
                     })
@@ -20,24 +39,46 @@ describe("APCB Blog Tests", () => {
             })
         })
     })
-    it("View All", () => {
-        cy.get('div.layout\\:right > div >div > div.article-card-group:nth-child(1) > div').each(($el, $index) => {
-            cy.get(`div.layout\\:right > div >div > div.article-card-group:nth-child(1) > div:nth-child(${$index + 1}) > h4`).then(($text) => {
-                cy.wrap($text).invoke('text').then(($cardTitle) => {
-                    let cardTitle = $cardTitle.trim();
-                    cy.get(`div.layout\\:right > div >div > div.article-card-group:nth-child(1) > div:nth-child(${$index + 1})`).click().then(() => {
-                        cy.get('h1').invoke('text').then(($text) => {
-                            let h1 = $text.trim();
-                            expect(h1).to.eq(cardTitle);
-                            cy.go('back')
+    it.skip("Verify Blog Cards on View All section are working", () => {
+        cy.get(`div.layout\\:right > div >div > div.article-card-group:nth-child(1) > div`).then(($blogs) => {
+            const blogCount = $blogs.length;
+
+            let randomCount = blogCount/2;
+
+            for(let i =0; i< randomCount; i++){
+                let randomIndex = Math.floor(Math.random() * blogCount);
+
+                if(randomIndex > 0){
+                    cy.get(`div.layout\\:right > div >div > div.article-card-group:nth-child(1) > div:nth-child(${randomIndex}) > h4`).then(($text) => {
+                        cy.wrap($text).invoke('text').then(($cardTitle) => {
+                            let cardTitle = $cardTitle.trim();
+                            cy.get(`div.layout\\:right > div >div > div.article-card-group:nth-child(1) > div:nth-child(${randomIndex})`).click().then(() => {
+                                cy.get('h1').invoke('text').then(($text) => {
+                                    let h1 = $text.trim();
+                                    expect(h1).to.eq(cardTitle);
+                                    cy.get('.raw.mb-8 > p:nth-child(1)').should('not.be.empty')
+                                    cy.go('back')
+                                })
+                            })
                         })
                     })
-                })
-            })
+                }                
+            }
+
         })
     })
-    it.only("By Tag", () => {
-        cy.get(`div.layout\\:right > div[data-ktc-search-exclude=""] > .flex > :nth-child(2)`).select('Benefits');
+    //inprogress
+    it.skip("Verify 'By Tag' filter is working", () => {
+        for(let i=0; i<blogFixture.tagsDropdown.length; i++){            
+            cy.get(`div.layout\\:right > div[data-ktc-search-exclude=""] > .flex > :nth-child(2)`).select(`${blogFixture.tagsDropdown[i]}`);
+            cy.get('h1').invoke('text').then(($h1) => {
+                let selectedTag = $h1.trim();
+                expect(selectedTag.toLowerCase()).to.eq(blogFixture.tagsDropdown[i].toLowerCase())
+            })
+        }
+    })
+    it.skip("Verify 'By Category' filter is working", () => {
+        cy.get(':nth-child(2) > .gap-3 > :nth-child(3)').select('Benefits');
         cy.get('h1').invoke('text').then(($h1) => {
             let selectedTag = $h1.trim();
         })
