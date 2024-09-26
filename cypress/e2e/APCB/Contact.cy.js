41c716174969700236624ede49b66697a481ff25
fads-innvovuzepc <fadspadilla@gmail.com>
2024-09-26 08:00:03 +0800

    Transfer Fixture to Contact Us POM

--- cypress/e2e/APCB/Contact.cy.js
+++ cypress/e2e/APCB/Contact.cy.js
@@ -1,23 +1,17 @@
 import Contact from "../../pages/APCB/contactObject.js"
 
 describe("APCB Contact Us Tests", () => {
-    let cfix;
     const contactObj = new Contact();
     beforeEach(() => {
         cy.visit("/")
         cy.get(".header-actions > a:nth-child(2)").click()
-        cy.fixture("APCB/contact.json").then((data) => {
-            cfix = data;
-        })
-
     })
 
     it("Verify that all 'Contact Us' input fields can successfully accept text", () => {
         cy.get('h3.mb-10').should('contain', 'Get In Touch Today')
-        contactObj.verifyInputFields(cfix.firstname, cfix.lastname, cfix.email, cfix.company, cfix.street, cfix.city, cfix.state, cfix.jobTitle, cfix.phoneNum, cfix.message)
+        contactObj.verifyInputFields()
     })
-    it.only("Verify if contact us displays validation errors on submit if required fields are empty", () => {
-        //verify if empty
-        contactObj.verifyIfEmpty(cfix.firstname, cfix.lastname, cfix.email, cfix.company, cfix.street, cfix.city, cfix.state, cfix.jobTitle, cfix.phoneNum, cfix.message)
+    it("Verify if contact us displays validation errors on submit if required fields are empty", () => {
+        contactObj.verifyIfEmpty()
     })
 })
--- cypress/pages/APCB/contactObject.js
+++ cypress/pages/APCB/contactObject.js
@@ -10,39 +10,42 @@ class Contact {
     phoneSeletor = "#phone-2e8942ba-e520-49ee-8129-b8af636087b6"
     msgSelector = "#message-2e8942ba-e520-49ee-8129-b8af636087b6"
     submitBtnSel = '.actions > input[value="Submit"]'
+    
 
-    verifyInputFields(fname, lname, email, company, street, city, state, jobtitle, phonenum, msg){
-        cy.get(this.fnameSelector).type(fname)
-        cy.get(this.fnameSelector).should('have.value', fname)
-
-        cy.get(this.lnameSelector).type(lname)
-        cy.get(this.lnameSelector).should('have.value', lname)
-
-        cy.get(this.emailSelector).type(email)
-        cy.get(this.emailSelector).should('have.value', email)
-
-        cy.get(this.comSelector).type(company)
-        cy.get(this.comSelector).should('have.value', company)
-
-        cy.get(this.addressSelector).type(street)
-        cy.get(this.addressSelector).should('have.value', street)
-
-        cy.get(this.citySelector).type(city)
-        cy.get(this.citySelector).should('have.value', city)
-
-        cy.get(this.sateSelector).type(state)
-        cy.get(this.sateSelector).should('have.value', state)
-        
-        cy.get(this.jobtitleSelector).type(jobtitle)
-        cy.get(this.jobtitleSelector).should('have.value', jobtitle)
-
-        cy.get(this.phoneSeletor).type(phonenum)
-        cy.get(this.phoneSeletor).should('have.value', phonenum)
-
-        cy.get(this.msgSelector).type(msg)
-        cy.get(this.msgSelector).should('have.value', msg)
+    verifyInputFields(){
+        cy.fixture("APCB/contact.json").then((data) => {            
+            cy.get(this.fnameSelector).type(data.firstname)
+            cy.get(this.fnameSelector).should('have.value', data.firstname)
+            
+            cy.get(this.lnameSelector).type(data.lastname)
+            cy.get(this.lnameSelector).should('have.value', data.lastname)
+            
+            cy.get(this.emailSelector).type(data.email)
+            cy.get(this.emailSelector).should('have.value', data.email)
+            
+            cy.get(this.comSelector).type(data.company)
+            cy.get(this.comSelector).should('have.value', data.company)
+            
+            cy.get(this.addressSelector).type(data.street)
+            cy.get(this.addressSelector).should('have.value', data.street)
+            
+            cy.get(this.citySelector).type(data.city)
+            cy.get(this.citySelector).should('have.value', data.city)
+            
+            cy.get(this.sateSelector).type(data.state)
+            cy.get(this.sateSelector).should('have.value', data.state)
+            
+            cy.get(this.jobtitleSelector).type(data.jobTitle)
+            cy.get(this.jobtitleSelector).should('have.value', data.jobTitle)
+            
+            cy.get(this.phoneSeletor).type(data.phoneNum)
+            cy.get(this.phoneSeletor).should('have.value', data.phoneNum)
+            
+            cy.get(this.msgSelector).type(data.message)
+            cy.get(this.msgSelector).should('have.value', data.message)
+        })
     }
-    verifyIfEmpty(fname, lname, email, company, street, city, state, jobtitle, phonenum, msg){
+    verifyIfEmpty(){
         cy.get(this.fnameSelector).should('have.value', '')
         cy.get(this.lnameSelector).should('have.value', '')
         cy.get(this.emailSelector).should('have.value', '')
