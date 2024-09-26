2815d8b95055f5b6564dd4e5e0a0c6e409ed984e
fads-innvovuzepc <fadspadilla@gmail.com>
2024-09-26 08:34:13 +0800

    Created Fixture and Object in APCB Header

--- cypress/e2e/APCB/Header.cy.js
+++ cypress/e2e/APCB/Header.cy.js
@@ -1,97 +1,23 @@
+import Header from "../../pages/APCB/HeaderObject.js";
+
 describe("APCB Header Tests", () => {
-    let headerFixture;
+    const headerObj = new Header();
     beforeEach(() => {
         cy.visit("/")
-        cy.fixture("APCB/header.json").then((data) => {
-            headerFixture = data;
-        })
     })
 
     it("Verify if services dropdown list redirects to corresponding page", () => {
-        cy.get("nav > ul > li:nth-child(2)").realHover();
-
-        // Check the dropdown list
-        cy.get("nav > ul > li:nth-child(2) > section > ul > li").each(($el, $index, $list) => {
-            if ($index != $list.length - 1) {
-                cy.get(`nav > ul > li:nth-child(2) > section > ul > li:nth-child(${$index + 1}) > a > span`).then(($text) => {
-                    cy.wrap($text).invoke('text').then(($links) => {
-                        let linkName = $links.trim();
-                        expect(linkName).to.eq(headerFixture.servicesDropdown[$index])
-                    })
-                })
-            }
-        })
-
-        for(let i=0; i < headerFixture.servicesDropdown.length; i++){
-            cy.get("nav > ul > li:nth-child(2)").realHover();
-            cy.get(`nav > ul > li:nth-child(2) > section > ul > li:nth-child(${i+1}) a`).then(($link) => {
-                cy.wrap(($link)).invoke('attr', 'href').then(($text) => {
-                    let href = $text.trim();
-                    cy.get(`nav > ul > li:nth-child(2) > section > ul > li:nth-child(${i+1}) a > span`).click({ force: true }).then(($link) => {
-                        cy.get('h1').should('contain', headerFixture.servicesDropdown[i])
-                        cy.url().should('include', href)
-                    })
-                })
-            })
-        }
+        headerObj.verifyServicesDropdown();
+        headerObj.clickServicesDropdown();
     })
 
     it("Verify if resources dropdown list redirects to corresponding page", () => {
-        cy.get("nav > ul > li:nth-child(3)").realHover();
-
-        //Check the dropdown list
-        cy.get("nav > ul > li:nth-child(3) > section > ul > li").each(($el, $index, $list) => {
-            if ($index != $list.length - 1) {
-                cy.get(`nav > ul > li:nth-child(3) > section > ul > li:nth-child(${$index + 1}) > a > span`).then(($text) => {
-                    cy.wrap($text).invoke('text').then(($links) => {
-                        let linkName = $links.trim();
-                        expect(linkName).to.eq(headerFixture.resourcesDropdown[$index])
-                    })
-                })
-            }
-        })
-
-        //navigate each dropdown list
-        for(let i=0; i < headerFixture.resourcesDropdown.length; i++){
-            cy.get("nav > ul > li:nth-child(3)").realHover();
-            cy.get(`nav > ul > li:nth-child(3) > section > ul > li:nth-child(${i+1}) a`).then(($link) => {
-                cy.wrap(($link)).invoke('attr', 'href').then(($text) => {
-                    let href = $text.trim();
-                    cy.get(`nav > ul > li:nth-child(3) > section > ul > li:nth-child(${i+1}) a > span`).click({ force: true }).then(($link) => {
-                        cy.get('h1').should('contain', headerFixture.resourcesDropdown[i])
-                        cy.url().should('include', href)
-                    })
-                })
-            })
-        }
+        headerObj.verifyServicesDropdown();
+        headerObj.clickResourcesDropdown();
     })
     it("Verify if company dropdown list redirects to corresponding page", () => {
-        cy.get("nav > ul > li:nth-child(4)").realHover();
+        headerObj.verifyCompanyDropdown();
+        headerObj.clickCompanyDropdown();
 
-        //Check the dropdown list
-        cy.get("nav > ul > li:nth-child(4) > section > ul > li").each(($el, $index, $list) => {
-            if ($index != $list.length - 1) {
-                cy.get(`nav > ul > li:nth-child(4) > section > ul > li:nth-child(${$index + 1}) > a > span`).then(($text) => {
-                    cy.wrap($text).invoke('text').then(($links) => {
-                        let linkName = $links.trim();
-                        expect(linkName).to.eq(headerFixture.companyDropdown[$index])
-                    })
-                })
-            }
-        })
-
-        //navigate each dropdown list
-        for(let i=0; i < headerFixture.companyDropdown.length; i++){
-            cy.get("nav > ul > li:nth-child(4)").realHover();
-            cy.get(`nav > ul > li:nth-child(4) > section > ul > li:nth-child(${i+1}) a`).then(($link) => {
-                cy.wrap(($link)).invoke('attr', 'href').then(($text) => {
-                    let href = $text.trim();
-                    cy.get(`nav > ul > li:nth-child(4) > section > ul > li:nth-child(${i+1}) a > span`).click({ force: true }).then(($link) => {
-                        cy.get('h1').should('contain', headerFixture.companyDropdown[i])
-                        cy.url().should('include', href)
-                    })
-                })
-            })
-        }
     })
 })
---
+++ cypress/pages/APCB/HeaderObject.js
@@ -0,0 +1,107 @@
+class Header {
+    verifyServicesDropdown(){
+        cy.fixture("APCB/header.json").then((data) => {
+            cy.get("nav > ul > li:nth-child(2)").realHover();
+
+            cy.get("nav > ul > li:nth-child(2) > section > ul > li").each(($el, $index, $list) => {
+                if ($index != $list.length - 1) {
+                    cy.get(`nav > ul > li:nth-child(2) > section > ul > li:nth-child(${$index + 1}) > a > span`).then(($text) => {
+                        cy.wrap($text).invoke('text').then(($links) => {
+                            let linkName = $links.trim();
+                            expect(linkName).to.eq(data.servicesDropdown[$index])
+                        })
+                    })
+                }
+            })
+        })
+    }
+
+    verifyResourcesDropdown(){
+        cy.fixture("APCB/header.json").then((data) => {
+            cy.get("nav > ul > li:nth-child(3)").realHover();
+
+            cy.get("nav > ul > li:nth-child(3) > section > ul > li").each(($el, $index, $list) => {
+                if ($index != $list.length - 1) {
+                    cy.get(`nav > ul > li:nth-child(3) > section > ul > li:nth-child(${$index + 1}) > a > span`).then(($text) => {
+                        cy.wrap($text).invoke('text').then(($links) => {
+                            let linkName = $links.trim();
+                            expect(linkName).to.eq(data.resourcesDropdown[$index])
+                        })
+                    })
+                }
+            })
+        })
+    }
+
+    verifyCompanyDropdown(){
+        cy.fixture("APCB/header.json").then((data) => {
+            cy.get("nav > ul > li:nth-child(4)").realHover();
+
+            cy.get("nav > ul > li:nth-child(4) > section > ul > li").each(($el, $index, $list) => {
+                if ($index != $list.length - 1) {
+                    cy.get(`nav > ul > li:nth-child(4) > section > ul > li:nth-child(${$index + 1}) > a > span`).then(($text) => {
+                        cy.wrap($text).invoke('text').then(($links) => {
+                            let linkName = $links.trim();
+                            expect(linkName).to.eq(data.companyDropdown[$index])
+                        })
+                    })
+                }
+            })
+        })
+    }
+
+    clickServicesDropdown(){
+        cy.fixture("APCB/header.json").then((data) => {
+            for(let i=0; i < data.servicesDropdown.length; i++){
+                cy.get("nav > ul > li:nth-child(2)").realHover();
+                cy.get(`nav > ul > li:nth-child(2) > section > ul > li:nth-child(${i+1}) a`).then(($link) => {
+                    cy.wrap(($link)).invoke('attr', 'href').then(($text) => {
+                        let href = $text.trim();
+                        cy.get(`nav > ul > li:nth-child(2) > section > ul > li:nth-child(${i+1}) a > span`).click({ force: true }).then(($link) => {
+                            cy.get('h1').should('contain', data.servicesDropdown[i])
+                            cy.url().should('include', href)
+                        })
+                    })
+                })
+            }
+        })
+    }
+
+    clickResourcesDropdown(){
+        cy.fixture("APCB/header.json").then((data) => {
+            for(let i=0; i < data.resourcesDropdown.length; i++){
+                cy.get("nav > ul > li:nth-child(3)").realHover();
+                cy.get(`nav > ul > li:nth-child(3) > section > ul > li:nth-child(${i+1}) a`).then(($link) => {
+                    cy.wrap(($link)).invoke('attr', 'href').then(($text) => {
+                        let href = $text.trim();
+                        cy.get(`nav > ul > li:nth-child(3) > section > ul > li:nth-child(${i+1}) a > span`).click({ force: true }).then(($link) => {
+                            cy.get('h1').should('contain', data.resourcesDropdown[i])
+                            cy.url().should('include', href)
+                        })
+                    })
+                })
+            }
+        })
+    }
+
+    clickCompanyDropdown(){
+        cy.fixture("APCB/header.json").then((data) => {
+            for(let i=0; i < data.companyDropdown.length; i++){
+                cy.get("nav > ul > li:nth-child(4)").realHover();
+                cy.get(`nav > ul > li:nth-child(4) > section > ul > li:nth-child(${i+1}) a`).then(($link) => {
+                    cy.wrap(($link)).invoke('attr', 'href').then(($text) => {
+                        let href = $text.trim();
+                        cy.get(`nav > ul > li:nth-child(4) > section > ul > li:nth-child(${i+1}) a > span`).click({ force: true }).then(($link) => {
+                            cy.get('h1').should('contain', data.companyDropdown[i]) //validate page name
+                            cy.url().should('include', href) //validate url
+                        })
+                    })
+                })
+            }
+        })
+    }
+
+    
+}
+
+export default Header;
